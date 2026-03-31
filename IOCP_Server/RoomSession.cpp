744d45c1ff9d8c4aef6c6dcba9142d00ff686410
HG <artisboom@naver.com>
2026-03-31 23:20:26 +0900

    방 세션 기능 추가 - 유저 추가 - 유저 제거 - 유저 추가 알림 - 유저 제거 알림 - Room 상태 추가 - 방 초기화

--- IOCP_Server/RoomSession.cpp
+++ IOCP_Server/RoomSession.cpp
@@ -1,19 +1,80 @@
 #include "RoomSession.h"
 
-RoomSession::RoomSession(uint8_t maxUserCount)
-	: mMaxUserCount(maxUserCount)
+RoomSession::RoomSession()
+	: mMaxUserCount(2)
 	, mRoomId(0)
-	, mCurPage(1)
-	, mCurUserCount(1)
+	, mCurPage(0)
 	, mHost(nullptr)
+	, mRoomState(RoomState::IDLE)
 {
 	mUsers.reserve(mMaxUserCount);
+}
+
+void RoomSession::Init(uint16_t maxUserCount)
+{
+	mMaxUserCount = maxUserCount;
+	mUsers.reserve(maxUserCount);
+	mRoomState = RoomState::ACTIVE;
+}
+
+void RoomSession::Clear()
+{
+	mMaxUserCount = 2;
+	mUsers.clear();
+	mRoomState = RoomState::IDLE;
+}
+
+bool RoomSession::AddUser(ClientSession* session)
+{
+	if (mRoomState != RoomState::ACTIVE)
+		return false;
+
+	if (mUsers.size() >= mMaxUserCount)
+		return false;
+
+	mUsers.push_back(session);
+
+	JoinNotify(session);
+
+	return true;
+}
+
+bool RoomSession::RemoveUser(ClientSession* session)
+{
+	auto it = std::find(mUsers.begin(), mUsers.end(), session);
+	if (it != mUsers.end()) {
+		std::iter_swap(it, mUsers.end() - 1);
+		mUsers.pop_back();
+
+		LeaveNotify(session);
+	}
+
+	if (mUsers.empty())
+		mRoomState = RoomState::CLOSING;
+
+	return mUsers.empty();
 }
 
 void RoomSession::BroadCast(ClientSession* excludeSession, const char* data, int length)
 {
 	SRWLockGuard lock(&mSrwLock);
+	BroadCastUnsafe(excludeSession, data, length);
+}
 
+void RoomSession::JoinNotify(ClientSession* joinSession)
+{
+	string joinMsg(joinSession->GetUsername() + " Joined the room.");
+	BroadCastUnsafe(joinSession, joinMsg.c_str(), joinMsg.size());
+}
+
+void RoomSession::LeaveNotify(ClientSession* leaveSession)
+{
+	string leaveMsg(leaveSession->GetUsername() + " left the room.");
+	BroadCastUnsafe(leaveSession, leaveMsg.c_str(), leaveMsg.size());
+}
+
+void RoomSession::BroadCastUnsafe(ClientSession* excludeSession, const char* data, int length)
+{
 	for (auto* session : mUsers)
 	{
 		if (session->IsValid() && session != excludeSession)
--- IOCP_Server/RoomSession.h
+++ IOCP_Server/RoomSession.h
@@ -2,27 +2,60 @@
 #include <vector>
 #include "ClientSession.h"
 #include "SRWLockGuard.h"
+#include "../Common/Packet.h"
 
 using namespace std;
 
+enum class RoomState {
+	IDLE,     // 비활성
+	ACTIVE,   // 활성
+	CLOSING,  // 종료 중
+};
+
 class RoomSession
 {
+private:
+	void BroadCastUnsafe(ClientSession* excludeSession, const char* data, int length);
+
+public:
+	RoomSession();
+	~RoomSession() = default;
+
+	void Init(uint16_t maxUserCount);
+
+	void SetRoomId(uint16_t id) { mRoomId = id; }
+	void SetRoomName(const string& name) { mName = name; }
+
+	void SetMaxUserCount(uint16_t maxUserCount) { mMaxUserCount = maxUserCount; }
+
+	uint16_t GetRoomId() const { return mRoomId; }
+	const string& GetRoomName() const { return mName; }
+	uint16_t GetMaxUserCount() const { return mMaxUserCount; }
+	uint16_t GetCurrentUserCount() const { return mUsers.size(); }
+
+	bool IsEmpty() const { return mUsers.size() < mMaxUserCount; }
+
+	bool AddUser(ClientSession* session);
+	bool RemoveUser(ClientSession* session);
+	void Clear();
+
+	void BroadCast(ClientSession* excludeSession, const char* data, int length);
+	void JoinNotify(ClientSession* joinSession);
+	void LeaveNotify(ClientSession* leaveSession);
+
 private:
 	uint16_t mRoomId;
 	string mName;
 
 	uint16_t mCurPage;
 
-	uint8_t mMaxUserCount;
-	uint8_t mCurUserCount;
+	uint16_t mMaxUserCount;
 
 	RoomSession* mHost;
 	vector<ClientSession*> mUsers;
 
+	atomic<RoomState> mRoomState;
+
 	SRWLOCK mSrwLock;
-public:
-	RoomSession(uint8_t maxUserCount);
-	~RoomSession();
-	void BroadCast(ClientSession* excludeSession, const char* data, int length);
 };
 
